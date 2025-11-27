4feefffdb1f08f72027c5fef0c660fd60141b8b9
akash <akashkavaliakashkavali@gmail.com>
2025-11-27 10:35:36 +0530

    Updated product service and controller files

--- src/main/java/com/akash/ecom_proj1/model/Product.java
+++ src/main/java/com/akash/ecom_proj1/model/Product.java
@@ -1,14 +1,10 @@
 package com.akash.ecom_proj1.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -28,9 +24,12 @@ public class Product {
     private String category;
 
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
-    private boolean available;
-    private int quantity;
+    private boolean productAvailable;
+    private int stockQuantity;
 
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageDate;
 }
