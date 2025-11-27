0d645cd5b2dfa753667ce15cc24a4f8d96c407e3
akashkavaliakashkavali <akashkavaliakashkavali@gmail.com>
2025-11-27 11:09:28 +0530

    Update Productservice.java

--- src/main/java/com/akash/ecom_proj1/service/Productservice.java
+++ src/main/java/com/akash/ecom_proj1/service/Productservice.java
@@ -4,9 +4,7 @@ import com.akash.ecom_proj1.model.Product;
 import com.akash.ecom_proj1.repo.Productrepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -22,22 +20,5 @@ public class Productservice {
     public Product getProductById(int id) {
         return repo.findById(id).orElse(null);
     }
-
-    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
-        return null;
-    }
-
-    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
-        product.setImageDate(imageFile.getBytes());
-        product.setImageName(imageFile.getOriginalFilename());
-        product.setImageType(imageFile.getContentType());
-        return repo.save(product);
-    }
-
-    public void deleteProduct(int id) {
-        repo.deleteById(id);
-    }
-
-
 
 }
